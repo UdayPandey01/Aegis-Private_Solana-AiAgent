cef07bb1ada0ea16e173cd8db0e725124ad20507
Uday Pandey <pandeyuday113@gmail.com>
2025-09-30 06:26:32 +0000

    small fix

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,6 +1,16 @@
 use borsh::{BorshDeserialize, BorshSerialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, BorshSerialize, BorshDeserialize, Clone, Hash)]
-pub struct AgentOutput {
-    pub message_length: u64,
+#[derive(Debug, BorshSerialize, BorshDeserialize,Serialize, Deserialize, Clone, Hash)]
+pub struct ArbitrageInput {
+    pub orca_sol_reserves : u64,
+    pub orca_usdc_reserves : u64,
+    pub raydium_sol_reserves : u64,
+    pub raydium_usdc_reserves : u64,
+    pub user_profit_threshold : u64
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+pub struct ArbitrageAction {
+    pub transaction_instructions : Vec<Vec<u8>>,
 }
