cc7197381fbf4abc5fddf0424ea4101ffc6dda7b
Uday Pandey <pandeyuday113@gmail.com>
2025-10-13 09:57:58 +0000

    Added frontend url to fix cors error

--- executor/src/main.ts
+++ executor/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || '*' || "https://aegis-private-solana-ai-agent.vercel.app/",
+    origin: process.env.FRONTEND_URL || '*' || "https://aegis-private-solana-ai-agent.vercel.app",
     credentials: true,
   });
   const port = process.env.PORT || 3001;
